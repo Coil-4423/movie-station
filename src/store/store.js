7e221defcef53df0ebded1353eb848e1279ce6a9
Takehito Sumimura <tsumimura@my.bcit.ca>
2024-11-11 20:00:27 -0800

    changed some color theme

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,3 @@
-// import {configureStore} from '@reduxjs/toolkit'
-// import {persistReducer, persistStore} from 'redux-persist'
-// import favMovieReducer from '../features/favMoviesSlice'
-
-// export const store = configureStore({
-//     reducer: {
-//         favMovie:favMovieReducer,
-//     },
-// })
-
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
